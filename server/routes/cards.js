ab1d60f7d23a1680673aa14362b902752dda3b1d
Andrew Njoo <adnjoo@gmail.com>
2022-03-23 21:19:28 -0700

    edit routes

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,7 +1,27 @@
-exports.test = async (req, res) => {
+const { DataTypes } = require("@sequelize/core");
+const sequelize = require("../models/index.js").sequelize;
+
+const Card = sequelize.define("card", {
+  front: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  back: {
+    type: DataTypes.STRING,
+  },
+  cardset: {
+    type: DataTypes.STRING,
+  },
+});
+
+exports.getcards = async (req, res) => {
   try {
-    res.status(200).send("test");
-  } catch (err) {
-    console.log(err);
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+    let cards = await Card.findAll({ raw: true });
+    console.log(cards);
+    res.status(200).send(cards);
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
   }
 };
