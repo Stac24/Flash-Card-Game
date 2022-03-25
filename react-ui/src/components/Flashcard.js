13c809d484d99cb7d7864a4605763fb207feb5ee
Stephanie Cox <stephanieacox4@gmail.com>
2022-03-24 17:57:42 -0700

    Front of flash cards displaying on home page

---
+++ react-ui/src/components/Flashcard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Flashcard({flashcard}) {
+    return (
+        <div>
+            {flashcard.front}
+        </div>
+    )
+};
+
+export default Flashcard;
--- react-ui/src/pages/HomePage.js
+++ react-ui/src/pages/HomePage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Flashcardgroup from '../components/Flashcardgroup';
 
 function HomePage() {
+    const[flashcards, setCards] = useState([]);
+
+    const loadCards = async () => {
+        const response = await fetch('/getcards');
+        const data = await response.json();
+        setCards(data);
+    }
+
+    useEffect(() => {
+        loadCards();
+    }, []);
+
     return (
         <>
             <h1>Home Page</h1>
+            <Flashcardgroup flashcards={flashcards}></Flashcardgroup>
         </>
     );
 }
