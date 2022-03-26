6a0a4c834aa2ec06e12d4bc67e30b343a15ceb1e
felzelle <calc@oregonstate.edu>
2022-03-26 14:27:55 -0400

    more work on the HighScoresPage.js using useState and useEffect hooks

--- react-ui/src/pages/HighScoresPage.js
+++ react-ui/src/pages/HighScoresPage.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HighScoresTable from '../components/highScores/HighScoreTable'
 
-const HighHighScoresPage = async() => {
-    const response = await fetch('/topTen');  // need to build a top ten route to return top ten highest players
+const HighScoresPage = async() => {
+    const [scores, setScores] = useState([])
+    
+    const loadScores = async() => {
+        const response = await fetch('/topTen'); // need to define this route
+        const data = await response.json();
+        setScores(data);
+    }
+
+    useEffect(() => {
+        loadScores()
+    }, []);)
+
 
     return (
         <>
             <h2>High Scores</h2>
-            <HighScoresTable highScores = {highScores}>
+            <HighScoresTable scores = {scores}>
             </HighScoresTable>
         </>
     )
