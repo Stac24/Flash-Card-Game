532128abe495e17bdf578a22516078371275a0a0
felzelle <calc@oregonstate.edu>
2022-03-25 22:17:02 -0400

    adding auth-nav.js to react-ui/src/components; holds a button to be added to navbar

---
+++ react-ui/src/components/auth-nav.js
@@ -0,0 +1,12 @@
+//https://auth0.com/blog/complete-guide-to-react-user-authentication/#Add-User-Authentication
+
+import React from 'react';
+import AuthenticationButton from './authentication-button';
+
+const AuthNav = () => (
+    <div className="navbar-nav ml-auto">
+        <AuthenticationButton />
+    </div>
+);
+
+export default AuthNav;
