d839c11aad04ac9bb58961143fd26b26fe6ea8b6
Stephanie Cox <stephanieacox4@gmail.com>
2022-03-25 19:23:58 -0700

    Added form to create a flashcard. Front/back ends not properly connected yet

--- react-ui/src/pages/CreateCardPage.js
+++ react-ui/src/pages/CreateCardPage.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function CreateCardPage() {
+export const CreateCardPage = () => {
+    const [front, setFront] = useState('');
+    const [back, setBack] = useState('');
+    const [cardSet, setCardSet] = useState('');
+    
+
+    const history = useHistory();
+
+    const addCard = async () => {
+        const newCard = {front, back, cardSet};
+        const response = await fetch('/createcard', {
+            method: 'POST',
+            body: JSON.stringify(newCard),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(response.status === 201){
+            alert("Successfully added flashcard!")
+        }else{
+            alert(`Failed to add flashcard, status code = ${response.status}`);
+        }
+        history.push('/');
+    };
+
+
     return (
-        <>
-            <h1>Create a flash card: </h1>
-        </>
+        <div>
+             <h1>Add a Flashcard</h1>
+            <div>
+                <label> Front </label>
+                <label> Back </label>
+                <label>Topic</label>
+              
+            </div>
+         
+            <input
+                type="text"
+                placeholder='Enter front here'
+                value={front}
+                onChange={e => setFront(e.target.value)}/>
+            <input
+                type="text"
+                placeholder='Enter back here'
+                value={back}
+                onChange={e => setBack(e.target.value)}/>
+
+            <input
+                type="text"
+                placeholder='Enter topic here'
+                value={cardSet}
+                onChange={e => setCardSet(e.target.value)}/>
+
+            <button
+                onClick={addCard}
+            >Add</button>
+        </div>
     );
-}
+    };
 
 export default CreateCardPage;
